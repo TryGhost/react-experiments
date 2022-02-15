dd3deb87717662fc8283d47809a67a0fd7a0f111
Josh McMillan <josh@joshmcmillan.com>
2022-02-15 15:08:22 +0000

    Add utility to generate JWT from staff access token

---
+++ members/src/utils/token.js
@@ -0,0 +1,14 @@
+import { SignJWT } from "jose";
+import { Buffer } from "buffer/";
+
+export const getTokenFromKey = async (key) => {
+  const [id, secret] = key.split(":");
+
+  const signer = new SignJWT({})
+    .setProtectedHeader({ kid: id, alg: "HS256" })
+    .setExpirationTime("5m")
+    .setIssuedAt()
+    .setAudience("/canary/admin/");
+
+  return await signer.sign(Buffer.from(secret, "hex"));
+};
