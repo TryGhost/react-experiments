95ab0be250f91bd545dafe715c42ed1bc5b25bd9
Josh McMillan <josh@joshmcmillan.com>
2022-03-07 17:40:33 +0000

    Add additional comments to MemberListPage

--- members/src/pages/MemberListPage.js
+++ members/src/pages/MemberListPage.js
@@ -23,16 +23,25 @@ import { Input } from "../component-library/form/Input";
 export const MemberListPage = () => {
   const siteUrl = useSelector((state) => state.config.siteUrl);
 
+  // This represents the page number we're currently retrieving from the API
   const [pageNumber, setPageNumber] = useState(1);
+
+  // We'll fill this up with members from the useGetMembersQuery hook as it provides us with results
   const [members, setMembers] = useState([]);
+
+  // This is some state that'll hold any filters we're using on the query – e.g. a search term
   const [filters, setFilters] = useState({});
+
+  // This is the rtk-query hook holding our member data
   const { isFetching, data } = useGetMembersQuery({
     page: pageNumber,
     filters,
   });
 
+  // This determines if it's possible for us to load any more content – i.e. if we're on the final page
   const canFetchMore = pageNumber < (data?.meta.pagination.pages || Infinity);
 
+  // This handles the virtualisation of the list, via react-virtual
   const parentRef = useRef();
   const virtualizer = useVirtual({
     size: data?.meta.pagination.total || 0,
@@ -40,6 +49,8 @@ export const MemberListPage = () => {
     estimateSize: useCallback(() => VIRTUALIZED_TABLE_ROW_HEIGHT, []),
   });
 
+  // This effect is run whenever `data` changes – i.e. whenever more content is loaded in from the API –
+  // and appends that new data to the `members` state variable
   useEffect(() => {
     if (!data) {
       return;
@@ -48,6 +59,11 @@ export const MemberListPage = () => {
     setMembers((members) => [...members, ...data.members]);
   }, [data]);
 
+  // This effect triggers the loading of more data when the user scrolls past the end of currently loaded
+  // members – it checks to see if the last item in view (found by getting the last item of virtualizer.virtualItems)
+  // has an index that is greater than the current number of items in the `members` array. If so, we've scrolled
+  // past the end of our loaded data, so it updates the `pageNumber` state variable which in turn triggers a
+  // request for fresh data.
   useEffect(() => {
     const [lastItem] = [...virtualizer.virtualItems].reverse();
 
@@ -60,6 +76,8 @@ export const MemberListPage = () => {
     }
   }, [canFetchMore, isFetching, members, virtualizer.virtualItems]);
 
+  // This effect is used to clear out the members array, reset the virtualizer, and set things up to retrieve fresh
+  // data. It's run whenever we change the filters we're using.
   useEffect(() => {
     if (Object.keys(filters).length === 0) {
       return;
